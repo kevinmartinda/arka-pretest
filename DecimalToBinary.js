8adeb6660f11593e60daac7239b8684a8daaecc4
kevinmartinda <kevin36.k3@gmail.com>
2019-06-15 17:31:41 +0700

    soalno6 done

---
+++ DecimalToBinary.js
@@ -0,0 +1,21 @@
+const decToBin = dec => {
+	let status = true
+	let x = 0
+	let bin = []
+	while(status){
+		x = dec / 2
+		if(x % 1 !== 0){
+			bin.push(1)
+			x-=0.5
+		}else{
+			bin.push(0)
+		}
+		dec = x
+		if(dec === 0){
+			status = false
+		}
+	}
+
+
+	return bin.reverse().join('')
+}
